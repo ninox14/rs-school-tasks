bc1f7f2c2b01db7ed772b25283f78c1eb75f25d5
NiNoX14 <hardstuckd5lul@gmail.com>
2021-09-27 14:09:24 +0300

    feat: finish book form

--- museum/index.js
+++ museum/index.js
@@ -12,3 +12,7 @@ progressSound.addEventListener('input', function () {
     this.style.background = `linear-gradient(to right, #710707 0%, #710707 ${value}%, #fff ${value}%, white 100%)`
 })
 
+
+function toggleForm() {
+    document.body.classList.toggle('active_form');
+}
