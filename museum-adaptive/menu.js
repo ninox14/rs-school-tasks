cea42d586708d584acd8b34b526cc568b8fa87e8
NiNoX14 <hardstuckd5lul@gmail.com>
2021-10-04 16:09:34 +0300

    fix: nav padding 1024 and closing of burger

--- museum-adaptive/menu.js
+++ museum-adaptive/menu.js
@@ -15,8 +15,13 @@ navButton.onclick = () => {
 
     function closeBurger(e) {
         const target = e.target;
-
-        if (!target.closest('.nav-toggle') && target != document.querySelector('.nav ul')) {
+        // console.log(target);
+        // console.log(!!target.closest('.socials__item > a'));
+        // console.log(!target.closest('.nav-toggle') && target != document.querySelector('.nav ul')
+        //     && (!target.closest('.nav.active') || target.classList.contains('nav__link') || !!target.closest('.socials__item > a')) );
+        // if (!target.closest('.nav-toggle') && target != document.querySelector('.nav ul')) {  !target.closest('.nav.active')
+        if (!target.closest('.nav-toggle') && target != document.querySelector('.nav ul')
+        && (!target.closest('.nav.active') || target.classList.contains('nav__link') || !!target.closest('.socials__item > a')) ) {
             nav.classList.remove('active');
             navButton.classList.remove('active');
             document.querySelector(".welcome__header").classList.remove('active');
