fa7f04ce9b88ac5ba0f15389b460bc2c7f726423
NiNoX14 <hardstuckd5lul@gmail.com>
2021-10-10 20:49:13 +0300

    feat: gallery animation

--- museum-dom/index.js
+++ museum-dom/index.js
@@ -1,58 +1,106 @@
+let images;
 
-function shuffle(array) {
-    let currentIndex = array.length, randomIndex;
 
-    // While there remain elements to shuffle...
-    while (currentIndex != 0) {
 
-        // Pick a remaining element...
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex--;
-
-        // And swap it with the current element.
-        [array[currentIndex], array[randomIndex]] = [
-            array[randomIndex], array[currentIndex]];
-    }
-    return array;
+function debounce(func, wait=20, immediate=true) {
+  let timeout;
+  return function () {
+    let context = this,
+      args = arguments;
+    let later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    let callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
 }
 
+function shuffle(array) {
+  let currentIndex = array.length,
+    randomIndex;
+
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+  return array;
+}
 
 function gallery() {
-    const pictureInnerContainer = document.querySelector('.gallery__inner');
-    while (pictureInnerContainer.firstChild) {
-        pictureInnerContainer.removeChild(pictureInnerContainer.firstChild);
-    }
-    let images = [
-        './assets/img/galery/galery1.jpg',
-        './assets/img/galery/galery2.jpg',
-        './assets/img/galery/galery3.jpg',
-        './assets/img/galery/galery4.jpg',
-        './assets/img/galery/galery5.jpg',
-        './assets/img/galery/galery6.jpg',
-        './assets/img/galery/galery7.jpg',
-        './assets/img/galery/galery8.jpg',
-        './assets/img/galery/galery9.jpg',
-        './assets/img/galery/galery10.jpg',
-        './assets/img/galery/galery11.jpg',
-        './assets/img/galery/galery12.jpg',
-        './assets/img/galery/galery13.jpg',
-        './assets/img/galery/galery14.jpg',
-        './assets/img/galery/galery15.jpg',
-    ]
-    shuffle(images);
+  const pictureInnerContainer = document.querySelector(".gallery__inner");
+  while (pictureInnerContainer.firstChild) {
+    pictureInnerContainer.removeChild(pictureInnerContainer.firstChild);
+  }
+  let images = [
+    "./assets/img/galery/galery1.jpg",
+    "./assets/img/galery/galery2.jpg",
+    "./assets/img/galery/galery3.jpg",
+    "./assets/img/galery/galery4.jpg",
+    "./assets/img/galery/galery5.jpg",
+    "./assets/img/galery/galery6.jpg",
+    "./assets/img/galery/galery7.jpg",
+    "./assets/img/galery/galery8.jpg",
+    "./assets/img/galery/galery9.jpg",
+    "./assets/img/galery/galery10.jpg",
+    "./assets/img/galery/galery11.jpg",
+    "./assets/img/galery/galery12.jpg",
+    "./assets/img/galery/galery13.jpg",
+    "./assets/img/galery/galery14.jpg",
+    "./assets/img/galery/galery15.jpg",
+  ];
+  shuffle(images);
 
-    for (let elem of images) {
-        const img = document.createElement('img');
-        img.classList.add('gallery__img')
-        img.src = elem;
-        img.alt = elem.replace(/^.*[\\\/]/, '');
-        pictureInnerContainer.append(img)
-    }
+  for (let elem of images) {
+    const img = document.createElement("img");
+    img.classList.add("gallery__img");
+    img.src = elem;
+    img.alt = elem.replace(/^.*[\\\/]/, "");
+    pictureInnerContainer.append(img);
+  }
+}
+
+function checkSlide (e) {
+	console.count(e);
+
+	images.forEach(item => {
+
+		const slideInAt =
+      window.scrollY + window.innerHeight - item.height / 3;
+
+		const imageBottom = item.parentNode.parentNode.offsetTop + item.offsetTop + item.height;
+		const isHalfShown =
+      slideInAt > item.parentNode.parentNode.offsetTop + item.offsetTop;
+		const isNotScrolledPast = window.scrollY < imageBottom;
+		if (isHalfShown && isNotScrolledPast) {
+			item.classList.add('active');
+		} else {
+			item.classList.remove('active');
+		}
+	});
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-    gallery();
+  gallery();
+	setTimeout( () => {
+		images = document.querySelectorAll(".gallery__img");
+		console.log(images);
+	}, 1000)
 });
+window.addEventListener("scroll", debounce(checkSlide));
+
+
+
 
 
 
