6e81963b5dcc48d29e0a06bc7bb3dd045999a22c
NiNoX14 <hardstuckd5lul@gmail.com>
2021-12-03 17:10:35 +0200

    refactor: initial file rename

---
+++ news-js/src/components/controller/appLoader.ts
@@ -0,0 +1,12 @@
+import Loader from './loader';
+
+class AppLoader extends Loader {
+
+  constructor() {
+    super('https://newsapi.org/v2/', {
+      apiKey: '0981b8805afb4385955ecab0edae2ac4', // получите свой ключ https://newsapi.org/
+    });
+  }
+}
+
+export default AppLoader;
