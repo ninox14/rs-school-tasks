1dd69d4c4672dcce6959aa54b544c1be7ee51457
NiNoX14 <hardstuckd5lul@gmail.com>
2021-12-05 23:01:20 +0200

    refactor: typescript transition

--- news-js/src/components/controller/appLoader.ts
+++ news-js/src/components/controller/appLoader.ts
@@ -1,7 +1,6 @@
 import Loader from './loader';
 
 class AppLoader extends Loader {
-
   constructor() {
     super('https://newsapi.org/v2/', {
       apiKey: '0981b8805afb4385955ecab0edae2ac4', // получите свой ключ https://newsapi.org/
