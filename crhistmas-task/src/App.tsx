093402ceb869ae4600c223b775aca4f71be30d2e
NiNoX14 <hardstuckd5lul@gmail.com>
2021-12-10 18:51:21 +0200

    feat: main sreen

--- crhistmas-task/src/App.tsx
+++ crhistmas-task/src/App.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
 function App() {
-  return <div className="App">lol</div>;
+  return (
+    <>
+      <div className="welcome text-center clear d-flex flex-column justify-center align-center">
+        <h1 className="welcome__title text-uppercase ">
+          Помогите бабушке нарядить елку
+        </h1>
+        <button className="welcome__button">Начать</button>
+      </div>
+    </>
+  );
 }
 
 export default App;
