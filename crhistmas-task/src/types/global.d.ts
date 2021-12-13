f5b2a206924e76a864d4526815874a7031811d29
NiNoX14 <hardstuckd5lul@gmail.com>
2021-12-13 16:37:28 +0200

    refactor: typing reference

--- crhistmas-task/src/types/global.d.ts
+++ crhistmas-task/src/types/global.d.ts
@@ -1,25 +1,2 @@
 /// <reference types="react-scripts" />
-/// <reference types="../react-app-env.d.ts"/>
-interface SelectOptionInterface {
-  value: string;
-  label: string;
-}
-
-interface ButtonPropsInterface {
-  className: string;
-  InnerSvg?: (props: SVGProps<SVGSVGElement>) => JSX.Element;
-  // callback: (e: Mo
-  value: ToyFormEnum[];
-}
-
-interface SvgComponentsInterface {
-  [svgKey: string]: (props: React.SVGProps<SVGSVGElement>) => JSX.Element;
-}
-
-enum ToyFormEnum {
-  SvgBall = 'ball',
-  SvgBell = 'bell',
-  SvgCone = 'cone',
-  SvgToy = 'toy',
-  SvgSnowflake = 'snow',
-}
+/// <reference path="../react-app-env.d.ts"/>
