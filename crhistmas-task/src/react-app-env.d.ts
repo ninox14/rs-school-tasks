0bce4b7973c820e248b22658009f4fbe7dce344a
NiNoX14 <hardstuckd5lul@gmail.com>
2021-12-13 16:37:50 +0200

    feat: button interfaces

--- crhistmas-task/src/react-app-env.d.ts
+++ crhistmas-task/src/react-app-env.d.ts
@@ -1,1 +1,34 @@
 /// <reference types="react-scripts" />
+
+// import { SVGProps } from 'react';
+
+interface SelectOptionInterface {
+  value: string;
+  label: string;
+}
+
+interface ButtonPropsInterface {
+  className: string;
+  InnerSvg?: (props: SVGProps<SVGSVGElement>) => JSX.Element;
+  callback: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  value?: string;
+}
+
+interface FormTypeButtonsInterface extends ButtonPropsInterface {
+  className?: string;
+  callback: (e: React.MouseEvent<HTMLButtonElement>, key: string) => void;
+}
+
+interface SvgComponentsInterface {
+  [svgKey: string]: (props: React.SVGProps<SVGSVGElement>) => JSX.Element;
+}
+
+enum ToyFormEnum {
+  SvgBall = 'ball',
+  SvgBell = 'bell',
+  SvgCone = 'cone',
+  SvgToy = 'toy',
+  SvgSnowflake = 'snow',
+}
+
+type ToyForm = 'SvgBall' | 'SvgBell' | 'SvgCone' | 'SvgToy' | 'SvgSnowflake';
