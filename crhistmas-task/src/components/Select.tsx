e3a202cb31094355605d8834233cc11dfd1295c4
NiNoX14 <hardstuckd5lul@gmail.com>
2021-12-14 21:34:04 +0200

    fix: fix transparent background styles

--- crhistmas-task/src/components/Select.tsx
+++ crhistmas-task/src/components/Select.tsx
@@ -13,27 +13,27 @@ const customSelectStyles: StylesConfig<SelectOptionInterface> = {
   container: (styles) => ({
     ...styles,
     fontFamily: 'Nunito',
-    fontSize: '13px',
-    letterSpacing: '0.3px',
+    // fontSize: '13px',
+    // letterSpacing: '0.3px',
   }),
   option: (styles, { isSelected, isFocused }) => ({
     ...styles,
     cursor: 'pointer',
     transition: 'background 0.3s ease-in-out',
     backgroundColor: isSelected
-      ? chroma('#111b1b').alpha(0.7).css()
+      ? chroma('#111b1b').brighten(0.2).css()
       : undefined,
     ':hover': {
       ...styles[':hover'],
       backgroundColor: isSelected
-        ? chroma('#111b1b').alpha(0.9).css()
+        ? chroma('#111b1b').darken(0.1).css()
         : '#234141',
     },
   }),
   menu: (styles) => ({
     ...styles,
     background:
-      'radial-gradient(118.88% 606.86% at 0% 0%, rgba(255, 255, 255, 0.2) 0%, rgba(157, 243, 255, 0.2) 100%)',
+      'radial-gradient(118.88% 606.86% at 0% 0%, #2a5e51 0%, #164d36 100%)',
   }),
   singleValue: (styles) => ({
     ...styles,
