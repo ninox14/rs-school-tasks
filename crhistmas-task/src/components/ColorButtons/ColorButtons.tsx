996c4e95c8d4d27aef87e171e8781b5fea993886
NiNoX14 <hardstuckd5lul@gmail.com>
2021-12-15 18:14:17 +0200

    feat: color button component

--- crhistmas-task/src/components/ColorButtons/ColorButtons.tsx
+++ crhistmas-task/src/components/ColorButtons/ColorButtons.tsx
@@ -1,20 +1,31 @@
 import { FC } from 'react';
 import { Button } from '../Button/Button';
-import { colorButtonCallback } from '../logic';
 import './ColorButtons.scss';
 
 export const possibleColors = ['white', 'yellow', 'red', 'blue', 'green'];
 
-export const ColorButtons: FC<{}> = ({}) => (
+type ColorButtonsProps = {
+  activeColorFilters: string[];
+  handleAddColorFilter: (color: string) => void;
+  handleDeleteColorFilter: (color: string) => void;
+};
+
+export const ColorButtons: FC<ColorButtonsProps> = ({
+  activeColorFilters,
+  handleAddColorFilter,
+  handleDeleteColorFilter,
+}) => (
   <>
     {possibleColors.map((color) => {
+      const isActive = activeColorFilters.includes(color);
+      const onClick = isActive
+        ? () => handleDeleteColorFilter(color)
+        : () => handleAddColorFilter(color);
       return (
         <Button
-          className={'color-button ' + color}
+          className={`color-button ${color} ${isActive && 'active'}`}
           key={color}
-          callback={(e) => {
-            colorButtonCallback(e);
-          }}
+          onClick={onClick}
         />
       );
     })}
