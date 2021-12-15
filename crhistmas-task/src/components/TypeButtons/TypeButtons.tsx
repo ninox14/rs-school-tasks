8ea3db8cf5d702c196445e51d2c831570a8ebc39
NiNoX14 <hardstuckd5lul@gmail.com>
2021-12-15 18:15:19 +0200

    feat: form state handling

--- crhistmas-task/src/components/TypeButtons/TypeButtons.tsx
+++ crhistmas-task/src/components/TypeButtons/TypeButtons.tsx
@@ -2,17 +2,23 @@ import { FC } from 'react';
 import { Button } from '../Button/Button';
 import { toys } from '../../assets/svg-comps';
 import './TypeButtons.scss';
-export const TypeButtons: FC<FormTypeButtonsInterface> = ({ callback }) => (
+export const TypeButtons: FC<FormTypeButtonsInterface> = ({
+  activeFormFilters,
+  handleAddFormFilter,
+  handleDeleteFormFilter,
+}) => (
   <>
     {Object.keys(toys).map((key) => {
+      const isActive = activeFormFilters.includes(key);
+      const onClick = isActive
+        ? () => handleDeleteFormFilter(key)
+        : () => handleAddFormFilter(key);
       return (
         <Button
-          className="form-type-button"
+          className={`form-type-button ${isActive && 'active'}`}
           InnerSvg={toys[key]}
           key={key}
-          callback={(e) => {
-            callback(e, key);
-          }}
+          onClick={onClick}
         />
       );
     })}
