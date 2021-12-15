6142bf6ae21edf22e0f0f20409ae463108348b35
NiNoX14 <hardstuckd5lul@gmail.com>
2021-12-15 19:40:14 +0200

    feat: add possibility to add inner elem

--- crhistmas-task/src/components/Button/Button.tsx
+++ crhistmas-task/src/components/Button/Button.tsx
@@ -4,8 +4,10 @@ export const Button: FC<ButtonPropsInterface> = ({
   className,
   InnerSvg,
   onClick = () => {},
+  InnerElem,
 }) => (
   <button className={className} onClick={onClick}>
     {InnerSvg ? <InnerSvg /> : null}
+    {InnerElem ? <InnerElem /> : null}
   </button>
 );
