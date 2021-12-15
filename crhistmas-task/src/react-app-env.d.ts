c8d169cbe6e9949c50d3fe3cdfd68eabb39a0f52
NiNoX14 <hardstuckd5lul@gmail.com>
2021-12-15 18:16:50 +0200

    feat: toy item typing and refactor of some interfaces

--- crhistmas-task/src/react-app-env.d.ts
+++ crhistmas-task/src/react-app-env.d.ts
@@ -10,25 +10,39 @@ interface SelectOptionInterface {
 interface ButtonPropsInterface {
   className: string;
   InnerSvg?: (props: SVGProps<SVGSVGElement>) => JSX.Element;
-  callback: (e: React.MouseEvent<HTMLButtonElement>) => void;
   value?: string;
+  onClick?: (e?: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
 interface FormTypeButtonsInterface extends ButtonPropsInterface {
   className?: string;
-  callback: (e: React.MouseEvent<HTMLButtonElement>, key: string) => void;
+  activeFormFilters: string[];
+  handleAddFormFilter: (typeForm: string) => void;
+  handleDeleteFormFilter: (typeForm: string) => void;
+  onClick?: (e?: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
 interface SvgComponentsInterface {
   [svgKey: string]: (props: React.SVGProps<SVGSVGElement>) => JSX.Element;
 }
 
-enum ToyFormEnum {
+/* enum ToyFormEnum {
   SvgBall = 'ball',
   SvgBell = 'bell',
   SvgCone = 'cone',
   SvgToy = 'toy',
   SvgSnowflake = 'snow',
-}
+} */
 
 type ToyForm = 'SvgBall' | 'SvgBell' | 'SvgCone' | 'SvgToy' | 'SvgSnowflake';
+
+interface ToyItemInterface {
+  num: string;
+  name: string;
+  count: string;
+  year: string;
+  shape: string;
+  color: string;
+  size: string;
+  favorite: boolean;
+}
