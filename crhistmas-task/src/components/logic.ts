ddb34c03e87ad7f1b92c0793ecd44f7c3b8997e8
NiNoX14 <hardstuckd5lul@gmail.com>
2021-12-16 15:58:40 +0200

    feat: min max range constanst from data

--- crhistmas-task/src/components/logic.ts
+++ crhistmas-task/src/components/logic.ts
@@ -1,6 +1,44 @@
-import React from 'react';
+// import React from 'react';
 import data from '../data';
 
+const initialData = data as ToyItemInterface[];
+
+const LS_SUFFIX = 'Saved';
+
+export const [itemCountMin, itemCountMax] = getItemsRanges('count');
+export const [itemYearMin, itemYearMax] = getItemsRanges('year');
+
 export const getData = () => {
-  return data;
+  return initialData;
 };
+console.log([itemCountMin, itemCountMax]);
+console.log([itemYearMin, itemYearMax]);
+export function getItemsRanges(key: keyof ToyItemInterface): number[] {
+  const lsItem = localStorage.getItem(key + LS_SUFFIX);
+  if (!lsItem) {
+    const [min, max] = [getMinByKey(key), getMaxByKey(key)];
+    localStorage.setItem(key + LS_SUFFIX, JSON.stringify([min, max]));
+    return [min, max];
+  } else {
+    return JSON.parse(lsItem);
+  }
+}
+
+function getMinByKey(key: keyof ToyItemInterface) {
+  let min = Infinity;
+  for (let item of initialData) {
+    if (+item[key] < min) {
+      min = +item[key];
+    }
+  }
+  return min;
+}
+function getMaxByKey(key: keyof ToyItemInterface) {
+  let max = -Infinity;
+  for (let item of initialData) {
+    if (+item[key] > max) {
+      max = +item[key];
+    }
+  }
+  return max;
+}
