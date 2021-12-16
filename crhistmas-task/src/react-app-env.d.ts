5db131b976e912523d77846c30e77f8587eee97e
NiNoX14 <hardstuckd5lul@gmail.com>
2021-12-16 17:00:37 +0200

    feat: statehooks typings

--- crhistmas-task/src/react-app-env.d.ts
+++ crhistmas-task/src/react-app-env.d.ts
@@ -1,12 +1,23 @@
 /// <reference types="react-scripts" />
 
-// import { SVGProps } from 'react';
+type ToyFormEng = 'SvgBall' | 'SvgBell' | 'SvgCone' | 'SvgToy' | 'SvgSnowflake';
+
+type ToyForm = 'шар' | 'колокольчик' | 'шишка' | 'фигурка' | 'снежинка';
+type ToySize = 'большой' | 'средний' | 'малый';
+type ToyColor = 'белый' | 'желтый' | 'красный' | 'синий' | 'зелёный';
+type SortValue =
+  | 'nameAscending'
+  | 'nameDescending'
+  | 'yearAscending'
+  | 'yearDescending'
+  | '';
+
 interface SvgComponentsInterface {
   [svgKey: string]: (props: React.SVGProps<SVGSVGElement>) => JSX.Element;
 }
 
 interface SelectOptionInterface {
-  value: string;
+  value: SortValue;
   label: string;
 }
 interface ToyItemInterface {
@@ -34,15 +45,19 @@ interface FormTypeButtonsInterface extends ButtonPropsInterface {
   handleDeleteFormFilter: (typeForm: ToyForm) => void;
   onClick?: (e?: React.MouseEvent<HTMLButtonElement>) => void;
 }
-
-interface ShopPropsInterface extends FilterPropsInterface {
-  // activeFormFilters: ToyForm[];
-  // handleAddFormFilter: (typeForm: ToyForm) => void;
-  // handleDeleteFormFilter: (typeForm: ToyForm) => void;
-  // activeColorFilters: ToyColor[];
-  // handleAddColorFilter: (color: ToyColor) => void;
-  // handleDeleteColorFilter: (color: ToyColor) => void;
+interface SizeButtonsPropsInterface {
+  activeSizeFilters: ToySize[];
+  handleAddSizeFilter: (size: ToySize) => void;
+  handleDeleteSizeFilter: (size: ToySize) => void;
 }
+
+interface ColorButtonsProps {
+  activeColorFilters: string[];
+  handleAddColorFilter: (color: ToyColor) => void;
+  handleDeleteColorFilter: (color: ToyColor) => void;
+}
+
+interface ShopPropsInterface extends FilterPropsInterface {}
 
 interface FilterPropsInterface {
   activeFormFilters: ToyForm[];
@@ -51,13 +66,20 @@ interface FilterPropsInterface {
   activeColorFilters: ToyColor[];
   handleAddColorFilter: (color: ToyColor) => void;
   handleDeleteColorFilter: (color: ToyColor) => void;
+  activeSizeFilters: ToySize[];
+  handleAddSizeFilter: (size: ToySize) => void;
+  handleDeleteSizeFilter: (size: ToySize) => void;
+  onlyFavourite: boolean;
+  setOnlyFavourite: React.Dispatch<React.SetStateAction<boolean>>;
+  setDefaultFilters: () => void;
+  saveFilters: () => void;
+  activeSort: SortValue;
+  handleSortChange: (sort: SortValue) => void;
+  itemCountRange: number[];
+  handleCountRangeChange: (range: number[]) => void;
+  itemYearRange: number[];
+  handleYearRangeChange: (range: number[]) => void;
 }
-
-type ColorButtonsProps = {
-  activeColorFilters: string[];
-  handleAddColorFilter: (color: ToyColor) => void;
-  handleDeleteColorFilter: (color: ToyColor) => void;
-};
 
 interface PossibleFormsInterface {
   SvgBall: ToyForm;
@@ -73,9 +95,3 @@ interface PossibleColorsInterface {
   синий: ToyColor;
   зелёный: ToyColor;
 }
-
-type ToyFormEng = 'SvgBall' | 'SvgBell' | 'SvgCone' | 'SvgToy' | 'SvgSnowflake';
-
-type ToyForm = 'шар' | 'колокольчик' | 'шишка' | 'фигурка' | 'снежинка';
-type ToySize = 'большой' | 'средний' | 'малый';
-type ToyColor = 'белый' | 'желтый' | 'красный' | 'синий' | 'зелёный';
