50fdf3afab5b0c7f70a523391f0cf8dda864df67
NiNoX14 <hardstuckd5lul@gmail.com>
2021-12-20 16:20:02 +0200

    fix: display values of loaded range states

--- crhistmas-task/src/components/RangeSlider/RangeSlider.tsx
+++ crhistmas-task/src/components/RangeSlider/RangeSlider.tsx
@@ -1,37 +1,18 @@
-import { useState } from 'react';
 import Slider, { Range, RangeProps } from 'rc-slider';
 import 'rc-slider/assets/index.css';
 import styles from './RangeSlider.module.scss';
 
-// interface SliderRangePropsExtInterface extends RangeProps {
-//   classPrefix?: string;
-// }
-
 const RangeWith = Slider.createSliderWithTooltip(Range);
+interface RangeSliderProps extends RangeProps {
+  filterState: number[];
+}
 
-export const RangeSlider = (props: RangeProps) => {
-  const defautlValues = props?.defaultValue
-    ? props.defaultValue
-    : [props.min || 0, props.max || 100];
-  // (props.value as number[]);
-
-  const [lesser, setLesser] = useState(defautlValues[0]);
-  const [bigger, setBigger] = useState(defautlValues[1]);
-
-  const updateSideIndicators = (data: number[]) => {
-    setLesser(data[0]);
-    setBigger(data[1]);
-  };
-
+export const RangeSlider = (props: RangeSliderProps) => {
   return (
     <div className={styles['range-wrapper']}>
-      <div
-        className={styles.output}
-        dangerouslySetInnerHTML={{ __html: lesser.toString() }}
-      ></div>
+      <div className={styles.output}>{props.filterState[0]}</div>
       <RangeWith
         {...props}
-        onChange={updateSideIndicators}
         tipFormatter={(value) => <span className={styles.tip}>{value}</span>}
         railStyle={{
           backgroundColor: '#ffffffc5',
@@ -43,10 +24,7 @@ export const RangeSlider = (props: RangeProps) => {
         ]}
         handleStyle={[{ backgroundColor: '#19331a', borderColor: '#7eb19b' }]}
       />
-      <div
-        className={styles.output}
-        dangerouslySetInnerHTML={{ __html: bigger.toString() }}
-      ></div>
+      <div className={styles.output}>{props.filterState[1]}</div>
     </div>
   );
 };
