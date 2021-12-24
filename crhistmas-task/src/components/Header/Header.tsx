b336a3f2603d9edaf19bdb3af04f176ab0fb5e3e
NiNoX14 <hardstuckd5lul@gmail.com>
2021-12-24 17:18:57 +0200

    feat: header

---
+++ crhistmas-task/src/components/Header/Header.tsx
@@ -0,0 +1,29 @@
+import { FC } from 'react';
+import './Header.scss';
+
+export const Header: FC<HeaderPropsInterface> = ({
+  setIsOnTree,
+  isOnStart,
+  isOnTree,
+  favourites,
+}) => (
+  <header className="header">
+    <nav className="nav">
+      <button
+        className={`nav__btn ${!isOnStart && !isOnTree ? 'active' : null}`}
+        onClick={() => setIsOnTree(false)}
+      >
+        Игрушки
+      </button>
+      <button
+        className={`nav__btn  ${isOnTree && 'active'}`}
+        onClick={() => setIsOnTree(true)}
+      >
+        Ёлка
+      </button>
+    </nav>
+    <div className="fav-count">
+      <span>{favourites.length}</span>
+    </div>
+  </header>
+);
