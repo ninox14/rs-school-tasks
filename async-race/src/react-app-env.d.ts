e0c4b1e3792eb778c2e54904df7d9e1a7802ad45
NiNoX14 <hardstuckd5lul@gmail.com>
2022-01-17 20:53:44 +0200

    feat: car interfaces

--- async-race/src/react-app-env.d.ts
+++ async-race/src/react-app-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="react-scripts" />
+
+interface CarInterface {
+  id: number;
+  color: string;
+  name: string;
+}
+
+interface CreateCarDTO {
+  color: string;
+  name: string;
+}
