c0ee4fbe0d3f9c8bfd297256f8a05e2c5d65887b
NiNoX14 <hardstuckd5lul@gmail.com>
2022-01-17 20:52:49 +0200

    feat: car component

---
+++ async-race/src/components/Car/Car.tsx
@@ -0,0 +1,35 @@
+import './Car.scss';
+import { FC } from 'react';
+
+import { ReactComponent as CarSvg } from '../../assets/car.svg';
+import { Button } from '../Button/Button';
+
+import garageS from '../../state/GarageState';
+
+interface CarPropsInterface {
+  name: string;
+  id: number;
+  color: string;
+}
+
+export const Car: FC<CarPropsInterface> = ({ color, name, id }) => {
+  return (
+    <div className="car">
+      <div className="car-top">
+        <Button label="select" />
+        <Button
+          label="delete"
+          onClick={() => {
+            garageS.deleteCar(id);
+          }}
+        />
+        <h4 className="car-name">{name}</h4>
+      </div>
+      <div className="car-track">
+        <Button label="S" className="car-btn car-btn__start secondary" />
+        <Button label="R" className="car-btn car-btn__reset secondary" />
+        <CarSvg fill={color} width={100} height={50} />
+      </div>
+    </div>
+  );
+};
